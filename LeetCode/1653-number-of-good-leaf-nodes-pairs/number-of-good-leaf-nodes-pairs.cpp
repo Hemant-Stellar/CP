571f00e65eaa7d59f2b0710080dbe621b9002751
Hemant Singh <75863203+Hemant-Stellar@users.noreply.github.com>
2024-07-18 21:49:18 +0530

    Time: 53 ms (41.27%) | Memory: 33.4 MB (66.83%) - LeetSync

---
+++ LeetCode/1653-number-of-good-leaf-nodes-pairs/number-of-good-leaf-nodes-pairs.cpp
@@ -0,0 +1,46 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int countPairs(TreeNode* root, int distance) {
+        int result = 0;
+        dfs(root, distance, result);
+        return result;
+    }
+    
+private:
+    vector<int> dfs(TreeNode* node, int distance, int& result) {
+        if (!node) return {};
+        if (!node->left && !node->right) return {0};
+        
+        vector<int> leftDistances = dfs(node->left, distance, result);
+        vector<int> rightDistances = dfs(node->right, distance, result);
+        
+        for (int ld : leftDistances) {
+            for (int rd : rightDistances) {
+                if (ld + rd + 2 <= distance) {
+                    result++;
+                }
+            }
+        }
+        
+        vector<int> newDistances;
+        for (int ld : leftDistances) {
+            if (ld + 1 < distance) newDistances.push_back(ld + 1);
+        }
+        for (int rd : rightDistances) {
+            if (rd + 1 < distance) newDistances.push_back(rd + 1);
+        }
+        
+        return newDistances;
+    }
+};

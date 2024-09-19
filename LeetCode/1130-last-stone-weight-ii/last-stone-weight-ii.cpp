9682fbd7d6c5f1409db979a4f4c5e77d8ec9f4c4
Hemant Singh <75863203+Hemant-Stellar@users.noreply.github.com>
2024-09-19 23:08:26 +0530

    Time: 3 ms (68.76%) | Memory: 11.1 MB (41.02%) - LeetSync

---
+++ LeetCode/1130-last-stone-weight-ii/last-stone-weight-ii.cpp
@@ -0,0 +1,19 @@
+class Solution {
+vector<vector<int>> dp;
+int solve(vector<int>& stones,int index, int cSum,int &target,int &sum){
+    if(cSum>=target|| index >= stones.size()){
+        return abs(sum-(2*cSum));
+    }
+    if(dp[index][cSum]!=-1) return dp[index][cSum];
+    dp[index][cSum] = min(solve(stones,index+1,cSum+stones[index],target,sum),solve(stones,index+1,cSum,target,sum));
+    return dp[index][cSum];
+}
+public:
+    int lastStoneWeightII(vector<int>& stones) {
+       int  n = stones.size();
+       int sum = accumulate(stones.begin(),stones.end(),0);
+       int target = ceil(sum/2);
+        dp.resize(n+1,vector<int>(target+1,-1));
+       return solve(stones,0,0,target,sum);
+    }
+};
